7c1e759ddc397d9bee5789f9582dd02352f1c891
NIKITA RUSAKOV <nikitarusakovgaga@mail.ru>
2022-06-28 10:32:56 +0300

    init and webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Test Work',
+      template: path.resolve(__dirname, './src/template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // img
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // CSS, Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
